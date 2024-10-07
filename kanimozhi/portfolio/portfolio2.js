620df023ccc31274f4dd52f26ebda4af74fb8e05
Kanimozhi0818 <kanimozhimeena77@gmail.com>
2024-10-07 18:43:30 +0530

    Update portfolio2.js

--- kanimozhi/portfolio/portfolio2.js
+++ kanimozhi/portfolio/portfolio2.js
@@ -1,4 +1,4 @@
-/================ toggle icon navbar ================/
+/*================ toggle icon navbar ================*/
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -7,7 +7,7 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
 
-/================ scroll section active link================/
+/*================ scroll section active link================*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -19,25 +19,27 @@ window.onscroll = () => {
         let id = sec.getAttribute('id');
 
         if (top >= offset && top < offset + height) {
-            navLinks.forEach(link => {
+            navLinks.forEach.apply(link => {
                 link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                /*================ sticky navbar 
                 if (link.getAttribute('href').includes(id)) {
                     link.classList.add('active');
-                }
+                }===========*/
             });
         }
     });
 
-    /================ sticky navbar ===========/
+    /*================ sticky navbar ===========*/
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
 
-    /================ remove toggle icon and navbar ================/
+    /*================ remove toggle icon and navbar ================*/
     menuIcon.classList.remove('fa-xmark');
     navbar.classList.remove('active');
 };
 
-/================ scroll reveal ================/
+/*================ scroll reveal ================*/
 ScrollReveal({
     distance: '80px',
     duration: 2000,
@@ -49,7 +51,7 @@ ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact 
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
-/================ Typed Js ================/
+/*================ Typed Js ================*/
 const typed = new Typed('.multiple-text', {
     strings: ['Frontend Developer', 'Web Designer', 'Web Developer'],
     typeSpeed: 70,
